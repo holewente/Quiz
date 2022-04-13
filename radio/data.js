e0c2331aaeef6bfd5599e484b396b2840e841768
holewente <103565600+holewente@users.noreply.github.com>
2022-04-13 10:32:38 +0200

    Add files via upload

---
+++ radio/data.js
@@ -0,0 +1,63 @@
+const kerdesek = [
+    {
+      id:1, 
+      question: "In Australian football, what is the maximum number of players allowed on the field at a time?",
+      answers: {
+        a: "36",
+        b: "28",
+        c: "40",
+        d: "14"
+      },
+      correctAnswer: "a",
+      foto:"kep1.jpg"
+    },
+    {
+      id:2, 
+      question: "Where can you find arnis being widely practiced?",
+      answers: {
+        a: "India",
+        b: "Suriname",
+        c: "The Philippines",
+        d: "China"
+      },
+      correctAnswer: "c",
+      foto:"kep2.jpg"
+
+    },
+    {
+      id:3, 
+      question: "What player was the first to win five straight Wimbledon tennis titles?",
+      answers: {
+        a: "Arthur Ashe",
+        b: "Andre Agassi",
+        c: "Bjorn BorgS",
+        d: "Roger Federer"
+      },
+      correctAnswer: "c",
+      foto:"kep3.jpg"
+    },
+    {
+      id:4,
+      question: "Who ran the first four-minute mile?",
+      answers: {
+        a: "Roger Bannister",
+        b: "Roger Moore",
+        c: "Roger Ramjet",
+        d: "Banastre Tarleton"
+      },
+      correctAnswer: "a",
+      foto:"kep4.jpg"
+    },
+    {
+      id:5,
+      question: "In polo, what is a period of play called?",
+      answers: {
+        a: "chukka",
+        b: "set",
+        c: "half",
+        d: "quarter"
+      },
+      correctAnswer: "a",
+      foto:"kep5.jpg"
+    }
+  ];
